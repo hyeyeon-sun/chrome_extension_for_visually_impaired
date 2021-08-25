01d0c83b2e0c750ff91977dd75f1f8716f72a76c
dldzm29 <red131729@khu.ac.kr>
2021-08-26 02:10:02 +0900

    manifest file 수정, cors 문제 해결하기 위해 site link 바꿈

--- TMP-HIK/main2.js
+++ TMP-HIK/main2.js
@@ -91028,19 +91028,23 @@ WError.prototype.cause = function we_cause(c)
 };
 
 },{"assert-plus":305,"core-util-is":339,"extsprintf":354,"util":253}],458:[function(require,module,exports){
-
-
 //dldzm
 const request = require('request');
 const deepai = require("deepai");
 const private_keys = 'bd06d8eb-777b-4434-afd3-fa45a152bc5b';
 deepai.setApiKey(private_keys);
 
-const headers = {
+const ic_headers = {
   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
   'X-Naver-Client-Id': '_GkEcIm4mJmHHwP9nrgC',
   'X-Naver-Client-Secret': 'O8OvXtUusS'
 };
+
+const ocr_headers = {
+  'Content-Type': 'multipart/form-data',
+  'Authorization': 'KakaoAK ddd979325e29ad84a8465a05b69cffb1'
+};
+
 
 const stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "him", "his", "himself", "her", "hers", "herself", "it", "its", "itself", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"];
 
@@ -91109,47 +91113,68 @@ var densecapAPI = async function (URL_LINK){
     return result;
 };
 
+
+// 동기함수를 만들 것.
+// densecapAPI가 가져오는 동시에 ocr이 돌아가길 바람
+// densecapAPI 옆에 await 함수로 시작할 예정
+var OCR = async function (imgSrc) {
+  var ocr_options = {
+    url: 'https://cors-anywhere.herokuapp.com/https://dapi.kakao.com/v2/vision/text/ocr',
+    method: 'POST',
+    headers: ocr_headers,
+    files : imgSrc
+  };
+
+  function ocr_callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+    else {
+      console.log(response , error , body);
+    }
+  };
+
+  request(ocr_options, ocr_callback);
+};
+
 (async function() {
   async function async_load2() {
     let images = document.getElementsByTagName('img');
     for (let i = 0; i < images.length; i++){
-      if( i % 10 === 0){
-        setTimeout(function(){console.log('work!');}, 1000);
-      }
-      else {
-        let ImgCptResult =  densecapAPI(images[i].src);
-        ImgCptResult
+      let ImgCptResult =  densecapAPI(images[i].src);
+      
+      OCR(images[i].src);
+      /*
+      ImgCptResult
+      .then((value) =>{
+          images[i].alt = value;
+          dataString = `source=en&target=ko&text=${value}`;
+      })
+      .then((val) => {
+        var options = {
+          url : 'https://imagecaptioningintranslation.herokuapp.com/https://openapi.naver.com/v1/papago/n2mt',
+          method : 'POST',
+          headers : ic_headers,
+          body : dataString
+        };
 
-        .then((value) =>{
-            images[i].alt = value;
-            dataString = `source=en&target=ko&text=${value}`;
-        })
-
-        .then((val) => {
-          var options = {
-            url : 'https://newimgcpt.herokuapp.com/https://openapi.naver.com/v1/papago/n2mt',
-            method : 'POST',
-            headers : headers,
-            body : dataString,
-            maxRedirects: images.length,
-          };
-
-          images[i].alt = request(options, async function callback(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              let result = JSON.parse(body);
-              images[i].alt = result['message']['result']['translatedText'];
-            }
-            else {
-              images[i].alt = "알 수 없는 이미지입니다.";
-            }
+        images[i].alt = request(options, async function callback(error, response, body) {
+          if (!error && response.statusCode == 200) {
+            let result = JSON.parse(body);
+            images[i].alt = result['message']['result']['translatedText'];
           }
-        );
-        console.log(i, images[i].alt);
-        })
-      }
+          else {
+            images[i].alt = "알 수 없는 이미지입니다.";
+          }
+        }
+      );
+      console.log(i, images[i].alt);
+      })
+      */
     }
   };
   window.attachEvent ? window.attachEvent('onload', async_load2) : window.addEventListener('load', async_load2, false);
 })();
+
 
 },{"deepai":340,"request":404}]},{},[458]);
