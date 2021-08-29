7b23d3adfa8ef0144383ba9ddb9b75eea8f83fcd
minkyeong <alsrud713@gmail.com>
2021-08-29 21:18:14 +0900

    ALT값 보내기 해결

--- OCR/new.js
+++ OCR/new.js
@@ -1,61 +1,87 @@
 const axios = require('axios');
 
-//const ocr = async function (link) {
+//참고: OCR함수를 async함수로 설정하면 post부분에서 에러나서 일반 함수로 설정함
+
+
+//OCR 함수의 역할: image[i].src 입력받아서 -> request.json 반환
 function ocr(link){
-  var request_json = {
-    "requests": [
-      {
-        "features": [
-          {
-            "type": "TEXT_DETECTION"
-          }
-        ],
-        "image": {
-          "source": {
-            "imageUri": link
+  //link를 활용하여 request보내기 위해 json 담을 변수 생성
+  if (link.split(';')[0]=='data:image/jpeg'){ //base64형식으로 받아와진다면
+    var request_json = {
+      "requests": [
+        {
+          "features": [
+            {
+              "type": "TEXT_DETECTION"
+            }
+          ],
+          "image": {
+            "content": link.split(',')[1]
           }
         }
-      }
-    ]
-  };
-
-  //request_json.requests[0].image.source.imageUri = "https://d2v80xjmx68n4w.cloudfront.net/gigs/DTX401601042005.jpg";
-  console.log(request_json.requests[0]);
-
-  const apiCall='https://vision.googleapis.com/v1/images:annotate?key=YOUR_KEY';
-
-  axios.post(apiCall, request_json).then((response) => {
-    console.log(JSON.stringify(response.data.responses[0].fullTextAnnotation.text));
-    return response.data.responses[0].fullTextAnnotation.text;
-  }).catch((e) => {
-    console.log(e.response);
-  });
+      ]
+    };
+  }
+  else if (link.split(':')[0]=='http' || link.split(':')[0]=='https'){
+    var request_json = {
+      "requests": [
+        {
+          "features": [
+            {
+              "type": "TEXT_DETECTION"
+            }
+          ],
+          "image": {
+            "source": {
+              "imageUri": link
+            }
+          }
+        }
+      ]
+    };
+  }
+  if (request_json) //request_json 파일이 제대로 생성되었다면
+    return request_json;
+  else //request파일이 생성되지 않았다면 링크가 잘못된 것
+    return 'wrong_link';
 }
 
+//ocr 함수 실행되는 부분
 (async function() {
   async function async_load2() {
+    //image 링크들 받아오기 위해 모든 이미지 태그 가진 데이터 저장
       let images = document.getElementsByTagName('img');
       for (let i = 0; i < images.length; i++){
-        //console.log(images[i].src);
-        let sent = ocr(images[i].src);
-        if (!sent){ //alt값이 없다면 출력
-          console.log('undefined response');
-        }
-        else{
-          images[i].alt = sent;
-          console.log('alt값:',images[i].alt);
+        image_link = images[i].src;
+
+        //image_link를 사용할 수 있을 경우에만 (배너아니고, 링크 주소 있고, http나 jpeg 포함일때)
+        if (!image_link.includes('banner') && image_link != "" && (image_link.includes('http') || image_link.includes('jpeg'))){
+          console.log(image_link);
+          //링크를 활용해 request_json 파일 생성
+          let request_json = ocr(image_link);
+          if (request_json == 'wrong_link')
+            continue;
+          else{
+            //POST보내는 부분 시작
+            const apiCall='https://vision.googleapis.com/v1/images:annotate?key=YOUR_KEY';
+            const res = await axios.post(apiCall, request_json);
+            //POST한 결과 result에 저장
+            let result = res.data.responses[0].fullTextAnnotation;
+            if(!result){
+              //result에 값이 없다면 정상적이지 않은 것
+              if (!images[i].alt){ //기존의 alt값이 없다면 아래 문구로 대체
+                images[i].alt = '글씨가 없거나 확인할 수 없는 이미지입니다';
+              }
+              //기존의 alt가 있는 경우 변경X, 추후 image captioning 부분과 합치기
+            }
+            else{
+              //result에 값이 있다면 정상적으로 받아온 것!
+              images[i].alt = result.text;
+            }
+            //console.log('alt값:', images[i].alt);
+          }
         }
       }
   };
   window.attachEvent ? window.attachEvent('onload', async_load2) : window.addEventListener('load', async_load2, false);
 })();
-
-// (async function() {
-//   async function async_load2() {
-//       let images = document.getElementsByTagName('img');
-//       for (let i = 0; i < images.length; i++){
-//         console.log(images[i].src);
-//       }
-//   };
-//   window.attachEvent ? window.attachEvent('onload', async_load2) : window.addEventListener('load', async_load2, false);
-// })();
--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## Image To Text, OCR 부분 브랜치입니다
 
-### 파일 설명
+### 🎨실행방법
+background.js 파일에서 ```YOUR_KEY``` 부분에 google api key를 입력한 후 OCR폴더 전체를 chrome://extensions에 업데이트
+
+
+### 🎨/OCR 내의 파일 설명
 💾 background.js: require를 사용하여 모듈을 쓰기 위해 browserify로 생성된 메인 js파일
 
 명령어: 
@@ -14,26 +18,14 @@
 
 💾 /src: 사용하지 않게된 소스코드
 
-
+### 🎨진행상황
 ---
-- 2021.08.22 진행상황
+- 2021.08.22
 
 1. getAuthToken을 사용하여 api 활용하는 부분 에러는 안나도록 했는데
 2. console.log 창에 아무것도 뜨지 않는 문제 발생
 
-- 2021.08.25 진행상황
+- 2021.08.25
 
 1. manifest.json에서 background 설정을 해주어 확장프로그램 페이지의 '뷰 검사 백그라운드 페이지'로 console 확인 가능하도록 수정함
 2. axios 사용하여 POST 제대로 동작되도록 구현 완료 -> response값 받아올 수 있다!
----
-### 로컬 환경 구축
-클라이언트 라이브러리 설치
-```
-npm install --save @google-cloud/vision
-```
-
-CMD에서 서비스 계정 키가 포함된 JSON 파일의 경로 설정
-```javascript
-//KEY_PATH: 경로
-set GOOGLE_APPLICATION_CREDENTIALS=KEY_PATH
-```
